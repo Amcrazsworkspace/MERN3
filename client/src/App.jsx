b4c23fece6a95bccbf7ce1bb062124751fe5c42a
amcrazseworkspace <amcrazseworkspace@gmail.com>
2024-02-02 19:26:17 +0530

    create pages and routers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,17 @@ import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
 import About from './pages/about';
 
+
 export default function App() {
   return <BrowserRouter>
+  <Headers />
   <Route>
     <Route path='/' element={<Home />} />
     <Route path='/sign-in' element={<SignIn />} />
     <Route path='/sign-up' element={<SignUp />} />
     <Route path='/about' element={<About />} />
     <Route path='/profile' element={<Profile />} />
-    
-    
-  </Route>
+    </Route>
   </BrowserRouter>
  
   
